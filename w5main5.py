e48d1006f1391126b4aa9bfac790ff4bd439afc4
HeeJi <gmlwl8466@naver.com>
2016-03-30 13:25:21 +0900

    w5main5
    201611069

---
+++ w5main5.py
@@ -0,0 +1,15 @@
+﻿def gradegg():
+    marksTmp=raw_input('input marks (0~100)')
+    g=int(marksTmp)
+    if 90<=g<=100:
+        print ("A")
+    if 80<=g<90:
+        print ("B")
+    if 70<=g<80:
+        print ("C")
+    if 60<=g<70:
+        print ("D")
+    if g<60:
+        print ("F")
+
+gradegg()
