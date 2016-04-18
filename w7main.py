6bf14f3fedc2d3093431d9775517ac55ee1fd1ba
HeeJi <gmlwl8466@naver.com>
2016-04-18 16:30:25 +0900

    w7main
    201611069

---
+++ w7main.py
@@ -0,0 +1,25 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+t1.shape("turtle")
+def drawSquareAtSave(size, pos):
+ t1.penup()
+ t1.setpos(pos)
+ t1.pendown()
+ tracks=list()
+ for i in range(0,4):
+  tracks.append(t1.pos())
+  t1.fd(size)
+  t1.right(90)
+ return tracks
+
+def lab7a():
+ mytrack=drawSquareAtSave(150, (100,100))
+ print mytrack
+    
+def main():
+ lab7a()
+
+main()
+raw_input("201611069")
+wn.exitonclick()
