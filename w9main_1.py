6c730ab428412dfa49cbaa58f6d22f39fd33fbb6
p2_201611069 <gmlwl8466@naver.com>
2016-05-06 12:29:37 +0900

    w9 homework

---
+++ w9main_1.py
@@ -0,0 +1,12 @@
+﻿import math
+Locations=tuple()
+Locations=[(37.571650, 126.976480),(37.570200, 126.982992),(37.576548, 126.985449),(37.574528, 126.957726),(37.563637, 126.975359)]
+gyung=(37.575825, 126.973473)
+d=list()
+for i in Locations:
+ distance=math.sqrt((i[0]-gyung[0])**2+(i[1]-gyung[1])**2)
+ d.append(distance)
+ print distance
+
+print 'the minimum is %s' %min(d)
+raw_input('201611069_w9main hw')
---
+++ w9main_2.py
@@ -0,0 +1,51 @@
+﻿people=list()
+people=[[74425,76326],[61164,61636],[109688,115744],[144796,146776],[174996,181676],[177841,177434],[204630,205980],[223373,232245],[161055,166130],[171576,176735],[279169,293772],[239450,251066],[148690,156510],[182977,196992],[237792,242641],[283869,296762],[209344,210282],[118965,114441],[186503,186856],[195734,203014],[254381,249472],[212401,229111],[271654,295354],[319197,335045],[229829,231671]]
+def man():
+ mansum=0
+ for i in range(0,len(people)):
+  mansum=mansum+people[i][0]
+ print mansum
+
+def woman():
+ womansum=0
+ for i in range(0,len(people)):
+  womansum=womansum+people[i][1]
+ print womansum
+
+def averman():
+ mansum=0
+ for i in range(0,len(people)):
+  mansum=mansum+people[i][0]
+ aver=mansum/len(people)
+ print aver
+
+def averwoman():
+ womansum=0
+ for i in range(0,len(people)):
+  womansum=womansum+people[i][1]
+ aver=womansum/len(people)
+ print aver
+
+def charGupeople():
+ p=dict()
+ for d in range(0,len(people)):
+  p[d]=people[d][0]+people[d][1]
+ print p
+ import matplotlib.pyplot as plt
+ plt.bar(range(len(p)), p.values(), align='center')
+ plt.xticks(range(len(p)), list(p.keys()))
+ plt.show()
+
+def lab9():
+ man()
+ woman()
+ averman()
+ averwoman()
+ charGupeople()
+
+def main():
+ lab9()
+
+main()
+if _name_=="_main_":
+ main()
