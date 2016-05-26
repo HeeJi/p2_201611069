eae092ebc8b7345eda0526b54f32a2cbef350387
p2_201611069 <gmlwl8466@naver.com>
2016-05-26 11:56:07 +0900

    w12main homework

--- w12main.py
+++ w12main.py
@@ -1,47 +1,46 @@
-﻿import os  
-mydir=os.getcwd() 
-def firstfile(): 
- filename='python.txt' 
- myfilename=os.path.join(mydir,filename) 
- try: 
-  myfile=open(myfilename, 'r') 
-  contents=myfile.readlines()
-  for line in myfile: 
-   if line.find('Python')>=0: 
-    print line 
-  myfile.close() 
- except IOError as e: 
-  print e
-
-sfilename='output.txt'
-smyfilename=os.path.join(mydir,sfilename)
-def secondfile():
- smyfile=open('output.txt','w')
+﻿import time
+import os
+def task_1():
+ myfile=open('output.txt','w')
  line1='first line\n'
- smyfile.write(line1)
- line2='\second line\n'
- smyfile.write(line2)
- line3='third line'
- smyfile.write(line3)
- smyfile.close()
- tmyfile=open('output.txt','r')
- scontents=tmyfile.readlines()
- for i in range(0,len(scontents)):
-  if scontents[i].find('line')>=0:
-   print 'line'.upper()
- tmyfile.close()
+ myfile.write(line1)
+ line2='\tsecond line\n'
+ myfile.write(line2)
+ line3='third'
+ myfile.write(line3)
+ myfile.close()
+ fin=open('output.txt','r')
+ fout=open('outputUpper.txt','w')
+ for line in fin:
+  words=line.split()
+  if line.find('line')>=0:
+   msg='[khj edited {0}]'.format(time.strftime('%Y-%m-%d %H:%M:%S'))
+   fout.write(msg)
+  for word in words:
+   if word=='line':
+    word=word.upper()
+   fout.write(word)
+   fout.write(' ')
+  fout.write('\n')
+ fin.close()
+ fout.close()
+
+def task_2():
+ data=[1,2,3,4,5,6,7,8,9,10]
+ fout=open('outputNumber.txt','w')
+ for i in data:
+  str='{0}\t'.format(i)
+  fout.write(str)
+  if not i%2:
+   fout.write('\n')
+ fout.close()
 
 def lab12():
- print 'w12main_1'
- firstfile()
- print 'w12main_2'
- secondfile()
+ task_1()
+ task_2()
 
 def main():
  lab12()
 
-if __name__=="__main__": 
+if __name_=="__main__":
  main() 
-          
-
-
