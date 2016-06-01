c829c4dee2065b30908ddf5f2a522f0abbc53c14
p2_201611069 <gmlwl8466@naver.com>
2016-06-02 00:06:29 +0900

    강아지울음소리
    클래스

---
+++ w14main.py
@@ -0,0 +1,38 @@
+﻿class Dog(object):
+    def __init__(self,name):
+        self.name=name
+    def getName(self):
+        print 'my dog name is ',self.name
+    def talk(self):
+        print 'normal dogs say : mung mung'
+
+class ShihTzuDog(object):
+    def __init__(self,name):
+        self.name=name
+    def talk(self):
+        print 'ShihTzu says : si si'
+
+class Maltese(object):
+    def __init__(self,name):
+        self.name=name
+    def talk(self):
+        print 'Maltese says : mal mal'
+
+def DogsSay():
+    mydog=Dog('dogs')
+    mydog.talk()
+    myshihtzu=ShihTzuDog('ShihTzu')
+    myshihtzu.talk()
+    mymaltese=Maltese('Maltese')
+    mymaltese.talk()
+
+def lab14():
+    DogsSay()
+
+def main():
+    lab14()
+
+if __name__=="__main__":
+ main()
+
+raw_input('201611069_w14 homework')
